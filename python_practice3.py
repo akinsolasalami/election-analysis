6a3242e71eea0698c04afa782d96725fb0af6878
Akin <akinsola@Akins-Mac-mini.local>
2021-09-29 07:29:58 -0400

    File 3 Revision

---
+++ python_practice3.py
@@ -0,0 +1,18 @@
+print("Hello World")
+
+# Order of precedence in python
+# Operations enclosed in parentheses are performed first.
+# Exponentiations (i.e., raising a number to a power) are performed next.
+# Multiplication and division operations are performed from left to right: *, /, //, and %.
+# Finally, addition and subtraction operations are performed from left to right.
+
+counties = ["Arapahoe","Denver","Jefferson"]
+
+print(counties)
+
+print(counties[0])
+
+# Negative indices are used to identify a lists item position relative to 
+# the end of the list. To identify the last item as shown below.
+
+print(counties[-1])
